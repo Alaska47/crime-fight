d15754816b7ee2c38c7dcbc3556d658bdeedcee2
Mihir Patel <mihir.v.patel7@gmail.com>
2017-03-25 18:25:11 -0400

    font

--- App/CrimeFighter/app/src/main/java/com/crimefighter/crimefighter/IntroActivity.java
+++ App/CrimeFighter/app/src/main/java/com/crimefighter/crimefighter/IntroActivity.java
@@ -1,13 +1,23 @@
 package com.crimefighter.crimefighter;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class IntroActivity extends AppCompatActivity {
+
+    private Typeface mTypeface;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        mTypeface = Typeface.createFromAsset(getAssets(),"fonts/montserrat.ttf");
+        mTextView = (TextView)findViewById(R.id.into_text_view);
+        mTextView.setTypeface(mTypeface);
+
     }
 }
