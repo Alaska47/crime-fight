94745c3a2a522d88cd0698b8bad6af7f17b8962f
Mihir Patel <mihir.v.patel7@gmail.com>
2017-03-25 19:24:18 -0400

    advance butotn

--- App/CrimeFighter/app/src/main/java/com/crimefighter/crimefighter/IntroActivity.java
+++ App/CrimeFighter/app/src/main/java/com/crimefighter/crimefighter/IntroActivity.java
@@ -1,23 +1,42 @@
 package com.crimefighter.crimefighter;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class IntroActivity extends AppCompatActivity {
 
     private Typeface mTypeface;
-    private TextView mTextView;
+    private TextView mTitleTextView;
+    private TextView mSubTextTextView;
+    private Button mButton;
 
+    public void advance() {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
         mTypeface = Typeface.createFromAsset(getAssets(),"fonts/montserrat.ttf");
-        mTextView = (TextView)findViewById(R.id.intro_title);
-        mTextView.setTypeface(mTypeface);
-
+        mTitleTextView = (TextView)findViewById(R.id.intro_title);
+        mTitleTextView.setTypeface(mTypeface);
+        mSubTextTextView = (TextView)findViewById(R.id.intro_subtext);
+        mSubTextTextView.setTypeface(mTypeface);
+        mButton = (Button)findViewById(R.id.intro_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                advance();
+            }
+        });
     }
 }
