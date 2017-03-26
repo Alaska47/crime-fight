2e50bfc8bd89d2b70c8e1e9b5476939527039110
Mihir Patel <mihir.v.patel7@gmail.com>
2017-03-26 00:45:04 -0400

    update

--- App/CrimeFighter/app/src/main/java/com/crimefighter/crimefighter/activities/StolenActivity.java
+++ App/CrimeFighter/app/src/main/java/com/crimefighter/crimefighter/activities/StolenActivity.java
@@ -1,14 +1,29 @@
 package com.crimefighter.crimefighter.activities;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.crimefighter.crimefighter.R;
 
 public class StolenActivity extends AppCompatActivity {
 
+    public static Typeface mTypeface;
+    private TextView mNameTextView;
+    private TextView mDistanceTextView;
+    private TextView mDescriptionTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mTypeface = Typeface.createFromAsset(getAssets(),"fonts/montserrat.ttf");
+        mNameTextView = (TextView)findViewById(R.id.stolen_name);
+        mNameTextView.setTypeface(mTypeface);
+        mDistanceTextView = (TextView)findViewById(R.id.stolen_distance);
+        mDistanceTextView.setTypeface(mTypeface);
+        mDescriptionTextView = (TextView)findViewById(R.id.stolen_description);
+        mDescriptionTextView.setTypeface(mTypeface);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stolen);
     }
